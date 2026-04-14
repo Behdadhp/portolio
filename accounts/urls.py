a3b36d977279f759013024c80c984ec22a0e307c
bhajipour <bhajipour@avodaq.com>
2026-04-14 21:34:10 +0200

    Update `accounts` URLs: set home view as default, move login view to `/login/`

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login_view, name="login"),
+    path("", views.home_view, name="home"),
+    path("login/", views.login_view, name="login"),
     path("register/", views.register_view, name="register"),
     path("dashboard/", views.dashboard_view, name="dashboard"),
     path("profile/edit/", views.edit_profile_view, name="edit_profile"),
