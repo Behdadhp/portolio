29fdcd236a3686109187817cfae9e419dfe997c3
bhajipour <bhajipour@avodaq.com>
2026-04-15 16:27:35 +0200

    Register `PriceAlert` model in the admin panel with list, filter, and search functionalities.

--- assets/admin.py
+++ assets/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Crypto, CryptoAsset, Stock, StockAsset
+from .models import Crypto, CryptoAsset, PriceAlert, Stock, StockAsset
 
 
 @admin.register(Crypto)
@@ -27,3 +27,11 @@ class StockAssetAdmin(admin.ModelAdmin):
     list_display = ("stock", "user", "price", "amount", "date", "status")
     list_filter = ("status",)
     search_fields = ("stock__name", "stock__symbol", "user__email")
+
+
+@admin.register(PriceAlert)
+class PriceAlertAdmin(admin.ModelAdmin):
+    list_display = ("user", "symbol", "target_price", "direction", "email_sent", "created_at")
+    list_filter = ("direction", "email_sent")
+    search_fields = ("user__email", "stock__symbol", "crypto__symbol")
+    readonly_fields = ("created_at",)
